5872828f072097385fc1a7b601971e90b7458850
Kanha Rathod <rathodkanha8810@gmail.com>
2024-10-05 11:43:07 +0530

    Remove unwanted code

--- FRONTEND/src/navigation/index.tsx
+++ FRONTEND/src/navigation/index.tsx
@@ -14,13 +14,14 @@ import { RootState } from "../config/Store";
 import { UserState } from "../reducer/AuthReducer";
 import { useSelector } from "react-redux";
 const Navigation = () => {
-
   interface ProtectedRouteProps extends RoutesProps {
     isAuthenticated: boolean;
     authenticationPath: string;
   }
 
-  const login: any = useSelector<RootState, UserState>((state: any) => state.userLogin);
+  const login: any = useSelector<RootState, UserState>(
+    (state: any) => state.userLogin
+  );
 
   // element = { defaultProtectedRouteProps.isAuthenticated || login.loginSuccess ? (<Navigate replace to="/dashboard" />) : item.component }
 
@@ -30,17 +31,16 @@ const Navigation = () => {
   };
 
   const Login = React.lazy(() => import("../components/Login/Login"));
-  const Dashboard = React.lazy(() => import("../pages/Dashboard/Dashboard"));
-  const Charging = React.lazy(() => import("../pages/Charging/Charging"));
-  const Station = React.lazy(() => import("../pages/Station/Station"));
+
   const Users = React.lazy(() => import("../pages/Users/Users"));
-  const Bookings = React.lazy(() => import("../pages/Bookings/Bookings"));
-  const VendorManagement = React.lazy(() => import("../pages/VendorManagement/VendorManagement"));
+
   const Feedback = React.lazy(() => import("../pages/Feedback/Feedback"));
-  const Feedbackdetails = React.lazy(() => import("../pages/Feedbackdetails/Feedbackdetails"));
-  const ChargingdetailsTab =  React.lazy(() => import("../pages/Charging/Chargin.details.tab"));
-  const ServiceMonitoring =  React.lazy(() => import("../pages/Service-Monitoring/Service.Monitoring"));
-  const CustomerDatabase = React.lazy(() => import("../pages/Customer-Database/Customer.Database"));
+  const Feedbackdetails = React.lazy(
+    () => import("../pages/Feedbackdetails/Feedbackdetails")
+  );
+  const ServiceMonitoring = React.lazy(
+    () => import("../pages/Service-Monitoring/Service.Monitoring")
+  );
 
   return (
     <>
@@ -49,11 +49,29 @@ const Navigation = () => {
           <Route
             path="/login"
             element={
-              <Suspense fallback={<></>}>{defaultProtectedRouteProps.isAuthenticated || login.loginSuccess ? (<Navigate replace to="/dashboard" />) : <Login />}</Suspense>
+              <Suspense fallback={<></>}>
+                {defaultProtectedRouteProps.isAuthenticated ||
+                login.loginSuccess ? (
+                  <Navigate replace to="/dashboard" />
+                ) : (
+                  <Login />
+                )}
+              </Suspense>
             }
           />
-          <Route path="/" element={defaultProtectedRouteProps.isAuthenticated || login.loginSuccess ? (<Navigate replace to="/dashboard" />) : <Navigate replace to="/login" />} />
-          <Route path="/"
+          <Route
+            path="/"
+            element={
+              defaultProtectedRouteProps.isAuthenticated ||
+              login.loginSuccess ? (
+                <Navigate replace to="/dashboard" />
+              ) : (
+                <Navigate replace to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/"
             element={
               <ProtectedRoute
                 {...defaultProtectedRouteProps}
@@ -61,21 +79,46 @@ const Navigation = () => {
               />
             }
           >
+            <Route
+              path="/users"
+              element={
+                <Suspense fallback={<></>}>
+                  {" "}
+                  <Users />{" "}
+                </Suspense>
+              }
+            />
 
-            <Route path="/dashboard" element={<Suspense fallback={<></>}> <Dashboard /> </Suspense>} />
-            <Route path="/charging" element={<Suspense fallback={<></>}> <Charging /> </Suspense>} />
-            <Route path="/charging-details" element={<Suspense fallback={<></>}> <ChargingdetailsTab /> </Suspense>} />
-            <Route path="/station" element={<Suspense fallback={<></>}> <Station /> </Suspense>} />
-            <Route path="/users" element={<Suspense fallback={<></>}> <Users /> </Suspense>} />
-            <Route path="/bookings" element={<Suspense fallback={<></>}> <Bookings /> </Suspense>} />
-            <Route path="/vendor-management" element={<Suspense fallback={<></>}> <VendorManagement /> </Suspense>} />
-            <Route path="/customer-database" element={<Suspense fallback={<></>}> <CustomerDatabase /> </Suspense>} />
-            <Route path="/feedback" element={<Suspense fallback={<></>}> <Feedback /> </Suspense>} />
-            <Route path="/feedbackdetails" element={<Suspense fallback={<></>}> <Feedbackdetails /> </Suspense>} />
-            <Route path="/service-monitoring" element={<Suspense fallback={<></>}> <ServiceMonitoring /> </Suspense>} />
+            <Route
+              path="/feedback"
+              element={
+                <Suspense fallback={<></>}>
+                  {" "}
+                  <Feedback />{" "}
+                </Suspense>
+              }
+            />
+            <Route
+              path="/feedbackdetails"
+              element={
+                <Suspense fallback={<></>}>
+                  {" "}
+                  <Feedbackdetails />{" "}
+                </Suspense>
+              }
+            />
+            <Route
+              path="/service-monitoring"
+              element={
+                <Suspense fallback={<></>}>
+                  {" "}
+                  <ServiceMonitoring />{" "}
+                </Suspense>
+              }
+            />
           </Route>
-        </Routes >
-      </div >
+        </Routes>
+      </div>
     </>
   );
 };
